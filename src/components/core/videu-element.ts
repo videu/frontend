bf392cd89e19a0dc3bb2448e19df1cb31f851b70
Felix Kopp <sandtler@sandtler.club>
2020-04-14 00:41:31 +0200

    Use i18next

--- src/components/core/videu-element.ts
+++ src/components/core/videu-element.ts
@@ -19,28 +19,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import i18next, {
+    StringMap,
+    TFunctionKeys,
+    TFunctionResult,
+    TOptions,
+} from 'i18next';
+import backend from 'i18next-xhr-backend';
 import { LitElement } from 'lit-element';
 
-interface LanguageMap {
-    [propName: string]: string;
-}
-
 /**
- * Any currently supported locale as an ISO 639-1 (2-letter) code.
+ * Get the currently active locale.
  */
-export type LocaleLike =
-    'de'
-    | 'en';
+export function getLocale(): string {
+    const locale = window.localStorage.getItem('locale') || navigator.language;
 
-export function getDefaultLocale(): LocaleLike {
-    let locale: string = navigator.language.substr(0, 2);
-
-    switch (locale) {
-        case 'de':
-        case 'en':
-            return locale;
-        default:
-            return 'en';
+    /* TODO: Add region-specific translations */
+    switch (locale.substr(0, 2)) {
+        case 'de': return 'de';
+        case 'en': return 'en';
+        default: return locale;
     }
 }
 
@@ -52,89 +50,80 @@ export function getDefaultLocale(): LocaleLike {
 export class VideuElement extends LitElement {
 
     /**
-     * An associative array of all localized strings in the current locale.
-     * The array key is the string id.  This get updated automatically when
-     * the static `locale` property is set.
-     */
-    private static _langMap: LanguageMap = {};
-
-    /**
      * The number formatter for the currently active locale.
      * This gets updated automatically when the static `locale` property
      * is set.
      */
-    private static _numberFormat: Intl.NumberFormat =
-        new Intl.NumberFormat(getDefaultLocale());
+    private static numberFormat: Intl.NumberFormat =
+        new Intl.NumberFormat(getLocale());
 
     /**
      * The date/time formatter for the currently active locale.
      * This gets updated automatically when the static `locale` propery
      * is set.
      */
-    private static _dateTimeFormat: Intl.DateTimeFormat =
-        new Intl.DateTimeFormat(getDefaultLocale());
+    private static dateTimeFormat: Intl.DateTimeFormat =
+        new Intl.DateTimeFormat(getLocale());
 
-    /** The current locale as an ISO 639-1 code. */
-    private static _locale: LocaleLike = getDefaultLocale();
-
-    /** Internal callback for locale change events. */
-    private _localeChangeCb: () => void;
+    /**
+     * Set the current locale for string translations.
+     *
+     * @param locale The new locale.
+     */
+    public static async setLocale(locale: string): Promise<void> {
+        window.localStorage.setItem('locale', locale);
+        this.numberFormat = new Intl.NumberFormat(locale);
+        this.dateTimeFormat = new Intl.DateTimeFormat(locale);
+        await i18next.changeLanguage(locale);
+    }
 
     public constructor() {
         super();
 
-        this._localeChangeCb = () => {
-            if (this.parentNode === null) {
-                /* Cleanup if we've become an orphan */
-                window.removeEventListener(
-                    'videu-locale-change',
-                    this._localeChangeCb
-                );
-            } else {
-                this.requestUpdate();
-            }
-        };
-        window.addEventListener(
-            'videu-locale-change',
-            this._localeChangeCb
-        );
+        this.onI18nextEvent = this.onI18nextEvent.bind(this);
     }
 
     /**
-     * The current locale as an ISO 639-1 (2-letter) code.
-     * To change this property, use the static setter.  This will update
-     * all elements globally.
+     * Called when the element is inserted into the DOM.
+     *
+     * @override
      */
-    public get locale(): LocaleLike {
-        return VideuElement._locale;
+    public connectedCallback() {
+        super.connectedCallback();
+
+        i18next.on('initialized', this.onI18nextEvent);
+        i18next.on('languageChanged', this.onI18nextEvent);
     }
 
-    /** The current locale as an ISO 639-1 (2-letter) code */
-    public static get locale(): LocaleLike {
-        return this._locale;
-    }
+    /**
+     * Called when the element is removed from the DOM.
+     *
+     * @override
+     */
+    public disconnectedCallback() {
+        super.disconnectedCallback();
 
-    public static set locale(locale: LocaleLike) {
-        this._locale = locale;
-
-
-        this._numberFormat = new Intl.NumberFormat(this._locale);
-        this._dateTimeFormat = new Intl.DateTimeFormat(this._locale);
-        throw new Error('Not implemented yet');
+        i18next.off('initialized', this.onI18nextEvent);
+        i18next.off('languageChanged', this.onI18nextEvent);
     }
 
     /**
      * Get an internationalized string in the currently active language by
      * its id.
      *
-     * @param stringId The string id.
+     * @param key The string resource key.
+     * @param opts Additional parameters passed to `i18next` (optional).
      * @return The localized string, or an empty string if it does not exist.
      */
-    protected _(stringId: string): string {
-        if (VideuElement._langMap[stringId] === undefined) {
-            return '';
+    protected t<
+        TResult extends TFunctionResult = string,
+        TKeys extends TFunctionKeys = string,
+        TInterpolationMap extends object = StringMap
+    >(key: TKeys | TKeys[], opts?: TOptions<TInterpolationMap> | string): TResult {
+        if (i18next.isInitialized) {
+            return i18next.t<TResult, TKeys, TInterpolationMap>(key, '', opts);
         } else {
-            return VideuElement._langMap[stringId];
+            return '' as TResult;
         }
     }
 
@@ -143,29 +132,48 @@ export class VideuElement extends LitElement {
      * This includes the decimal comma/point or the point/comma for every
      * third order of magnitude.
      *
-     * @param n The number.
+     * @param num The number.
      * @return The string representation of `n`, in localized format.
      */
-    protected _n(n: number): string {
-        return VideuElement._numberFormat.format(n);
+    protected n(num: number): string {
+        return VideuElement.numberFormat.format(num);
     }
 
     /**
-     * Get a date string in the current locale format.
+     * Get a date and time string in the current locale format.
      *
      * @param date The date, or its UNIX timestamp representation in msecs.
      * @return The localized date string.
      */
-    protected _dt(date: Date | number): string {
+    protected dt(date: Date | number): string {
         /*
          * TODO: Add support for completely customized date formats
          *       in printf-style (somewhere in settings)
          */
         if (typeof date === 'number') {
-            return VideuElement._dateTimeFormat.format(new Date(date));
+            return VideuElement.dateTimeFormat.format(new Date(date));
         } else {
-            return VideuElement._dateTimeFormat.format(date);
+            return VideuElement.dateTimeFormat.format(date);
         }
     }
 
+    /**
+     * Callback for events emitted from `i18next`.
+     */
+    private onI18nextEvent() {
+        this.requestUpdate();
+    }
+
 }
+
+i18next.use(backend);
+i18next.init({
+    debug: window._videu.env === 'development',
+    lng: getLocale(),
+    defaultNS: 'core',
+    ns: ['core'],
+    fallbackLng: 'en',
+    backend: {
+        loadPath: '/intl/{{lng}}/{{ns}}.json',
+    },
+});
